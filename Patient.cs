093d39267db9d3a885fead9d99af121e7b06162f
Aryan0326 <127589987+Aryan0326@users.noreply.github.com>
2023-06-23 22:35:07 -0400

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Patient
+{
+    public string FullName { get; set; }
+    public double Weight { get; set; }
+    public double Height { get; set; }
+
+    public double CalculateBMI()
+    {
+        double heightInMeters = Height / 100; // Convert height from centimeters to meters
+        double bmi = Weight / (heightInMeters * heightInMeters);
+        return bmi;
+    }
+
+    public string GetBMIStatus()
+    {
+        double bmi = CalculateBMI();
+        
+        if (bmi >= 25.0)
+            return "Overweight";
+        else if (bmi >= 18.5 && bmi <= 24.9)
+            return "Normal (In the healthy range)";
+        else
+            return "Underweight";
+    }
+
+    public void PrintPatientInformation()
+    {
+        double bmi = CalculateBMI();
+        string bmiStatus = GetBMIStatus();
+
+        Console.WriteLine("Patient Information:");
+        Console.WriteLine("Full Name: " + FullName);
+        Console.WriteLine("Weight: " + Weight + " kg");
+        Console.WriteLine("Height: " + Height + " cm");
+        Console.WriteLine("Blood Pressure: " + GetBloodPressure());
+        Console.WriteLine("BMI: " + bmi);
+        Console.WriteLine("BMI Status: " + bmiStatus);
+    }
+
+    private string GetBloodPressure()
+    {
+        // Implement your logic to calculate the blood pressure here
+        // This is just a placeholder method
+        return "Normal";
+    }
+}
